8377fdb0db86320fdf653841186fc7ccd9f73440
ValentinEfimov <valentinefimov00@gmail.com>
2020-11-08 23:47:20 +0300

    ADDED: polynomial counting and main grid with buttons

--- src/bsu/rfe/java/group7/lab3/Efimov/varB5/MainFrame.java
+++ src/bsu/rfe/java/group7/lab3/Efimov/varB5/MainFrame.java
@@ -2,7 +2,21 @@ package bsu.rfe.java.group7.lab3.Efimov.varB5;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame {
     private static final int WIDTH = 700;
     private static final int HEIGHT = 500;
@@ -44,23 +58,189 @@ public class MainFrame extends JFrame {
         setJMenuBar(menuBar);
 // Добавить в меню пункт меню "Файл"
         JMenu fileMenu = new JMenu("Файл");
+        JMenu SpravkaMenu = new JMenu("Справка");
 // Добавить его в главное меню
         menuBar.add(fileMenu);
+        menuBar.add(SpravkaMenu);
 // Создать пункт меню "Таблица"
         JMenu tableMenu = new JMenu("Таблица");
+
 // Добавить его в главное меню
         menuBar.add(tableMenu);
 
 
 
+        Action searchValueAction = new AbstractAction("Найти значение многочлена") {
+            public void actionPerformed(ActionEvent event) {
+// Запросить пользователя ввести искомую строку
+                String value =
+                        JOptionPane.showInputDialog(MainFrame.this, "Введите значение для поиска",
+                                "Поиск значения", JOptionPane.QUESTION_MESSAGE);
+// Установить введенное значение в качестве иголки
+                renderer.setNeedle(value);
+// Обновить таблицу
+                getContentPane().repaint();
+            }
+        };
+// Добавить действие в меню "Таблица"
+        searchValueMenuItem = tableMenu.add(searchValueAction);
+// По умолчанию пункт меню является недоступным (данных ещѐ нет)
+        searchValueMenuItem.setEnabled(false);
+// Создать область с полями ввода для границ отрезка и шага
+// Создать подпись для ввода левой границы отрезка
+        JLabel labelForFrom = new JLabel("X изменяется на интервале от:");
+// Создать текстовое поле для ввода значения длиной в 10 символов
+// со значением по умолчанию 0.0
+        textFieldFrom = new JTextField("0.0", 10);
+// Установить максимальный размер равный предпочтительному, чтобы
+// предотвратить увеличение размера поля ввода
+        textFieldFrom.setMaximumSize(textFieldFrom.getPreferredSize());
+// Создать подпись для ввода левой границы отрезка
+        JLabel labelForTo = new JLabel("до:");
+// Создать текстовое поле для ввода значения длиной в 10 символов
+// со значением по умолчанию 1.0
+        textFieldTo = new JTextField("1.0", 10);
+// Установить максимальный размер равный предпочтительному, чтобы
+// предотвратить увеличение размера поля ввода
+        textFieldTo.setMaximumSize(textFieldTo.getPreferredSize());
+// Создать подпись для ввода шага табулирования
+        JLabel labelForStep = new JLabel("с шагом:");
+// Создать текстовое поле для ввода значения длиной в 10 символов
+// со значением по умолчанию 1.0
+        textFieldStep = new JTextField("0.1", 10);
+// Установить максимальный размер равный предпочтительному, чтобы
+// предотвратить увеличение размера поля ввода
+        textFieldStep.setMaximumSize(textFieldStep.getPreferredSize());
+// Создать контейнер 1 типа "коробка с горизонтальной укладкой"
+        Box hboxRange = Box.createHorizontalBox();
+// Задать для контейнера тип рамки "объѐмная"
+        hboxRange.setBorder(BorderFactory.createBevelBorder(1));
+// Добавить "клей" C1-H1
+        hboxRange.add(Box.createHorizontalGlue());
+// Добавить подпись "От"
+        hboxRange.add(labelForFrom);
+// Добавить "распорку" C1-H2
+        hboxRange.add(Box.createHorizontalStrut(10));
+// Добавить поле ввода "От"
+        hboxRange.add(textFieldFrom);
+// Добавить "распорку" C1-H3
+        hboxRange.add(Box.createHorizontalStrut(20));
+// Добавить подпись "До"
+        hboxRange.add(labelForTo);
+// Добавить "распорку" C1-H4
+        hboxRange.add(Box.createHorizontalStrut(10));
+// Добавить поле ввода "До"
+        hboxRange.add(textFieldTo);
+// Добавить "распорку" C1-H5
+        hboxRange.add(Box.createHorizontalStrut(20));
+// Добавить подпись "с шагом"
+        hboxRange.add(labelForStep);
+// Добавить "распорку" C1-H6
+        hboxRange.add(Box.createHorizontalStrut(10));
+// Добавить поле для ввода шага табулирования
+        hboxRange.add(textFieldStep);
+// Добавить "клей" C1-H7
+        hboxRange.add(Box.createHorizontalGlue());
+// Установить предпочтительный размер области равным удвоенному
+// минимальному, чтобы при компоновке область совсем не сдавили
+        hboxRange.setPreferredSize(new Dimension(new Double(hboxRange.getMaximumSize().getWidth()).intValue(),
+                new Double(hboxRange.getMinimumSize().getHeight()).intValue()*2));
+// Установить область в верхнюю (северную) часть компоновки
+        getContentPane().add(hboxRange, BorderLayout.NORTH);
+// Создать кнопку "Вычислить"
+        JButton buttonCalc = new JButton("Вычислить");
+// Задать действие на нажатие "Вычислить" и привязать к кнопке
+        buttonCalc.addActionListener(new ActionListener() {
 
+            public void actionPerformed(ActionEvent ev) {
+                try {
+// Считать значения начала и конца отрезка, шага
+                    Double from =
+                            Double.parseDouble(textFieldFrom.getText());
+                    Double to =
+                            Double.parseDouble(textFieldTo.getText());
+                    Double step =
+                            Double.parseDouble(textFieldStep.getText());
+// На основе считанных данных создать новый экземпляр модели таблицы
+                    data = new GornerTableModel(from, to, step,
+                            MainFrame.this.coefficients);
+// Создать новый экземпляр таблицы
+                    JTable table = new JTable(data);
+// Установить в качестве визуализатора ячеек для класса Double разработанный визуализатор
+                    table.setDefaultRenderer(Double.class,
+                            renderer);
+// Установить размер строки таблицы в 30 пикселов
+                    table.setRowHeight(30);
+// Удалить все вложенные элементы из контейнера
 
-
-
-
-
-
-
-
+                    hBoxResult.removeAll();
+// Добавить в hBoxResult таблицу, "обѐрнутую" в панель с полосами прокрутки
+                    hBoxResult.add(new JScrollPane(table));
+// Обновить область содержания главного окна
+                    getContentPane().validate();
+// Пометить ряд элементов меню как доступных
+                    saveToTextMenuItem.setEnabled(true);
+                    saveToGraphicsMenuItem.setEnabled(true);
+                    searchValueMenuItem.setEnabled(true);
+                } catch (NumberFormatException ex) {
+// В случае ошибки преобразования чисел показать сообщение об ошибке
+                    JOptionPane.showMessageDialog(MainFrame.this,
+                            "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+// Создать кнопку "Очистить поля"
+        JButton buttonReset = new JButton("Очистить поля");
+// Задать действие на нажатие "Очистить поля" и привязать к кнопке
+        buttonReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+// Установить в полях ввода значения по умолчанию
+                textFieldFrom.setText("0.0");
+                textFieldTo.setText("1.0");
+                textFieldStep.setText("0.1");
+                hBoxResult.removeAll();
+// Добавить в контейнер пустую панель
+                hBoxResult.add(new JPanel());
+// Пометить элементы меню как недоступные
+                saveToTextMenuItem.setEnabled(false);
+                saveToGraphicsMenuItem.setEnabled(false);
+                searchValueMenuItem.setEnabled(false);
+// Обновить область содержания главного окна
+                getContentPane().validate();
+            }
+        });
+// Поместить созданные кнопки в контейнер
+        Box hboxButtons = Box.createHorizontalBox();
+        hboxButtons.setBorder(BorderFactory.createBevelBorder(1));
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.add(buttonCalc);
+        hboxButtons.add(Box.createHorizontalStrut(30));
+        hboxButtons.add(buttonReset);
+        hboxButtons.add(Box.createHorizontalGlue());
+// Установить предпочтительный размер области равным удвоенному минимальному, чтобы при
+// компоновке окна область совсем не сдавили
+        hboxButtons.setPreferredSize(new Dimension(new
+                Double(hboxButtons.getMaximumSize().getWidth()).intValue(), new
+                Double(hboxButtons.getMinimumSize().getHeight()).intValue()*2));
+// Разместить контейнер с кнопками в нижней (южной) области граничной компоновки
+        getContentPane().add(hboxButtons, BorderLayout.SOUTH);
+// Область для вывода результата пока что пустая
+        hBoxResult = Box.createHorizontalBox();
+        hBoxResult.add(new JPanel());
+// Установить контейнер hBoxResult в главной (центральной) области граничной компоновки
+        getContentPane().add(hBoxResult, BorderLayout.CENTER);
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
