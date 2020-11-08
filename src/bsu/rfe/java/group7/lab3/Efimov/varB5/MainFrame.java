a6e67276fe2c3919df81832d34edb48725210b6e
ValentinEfimov <valentinefimov00@gmail.com>
2020-11-08 19:36:12 +0300

    ADDED: Menu bar in MainFrame  constractor

--- src/bsu/rfe/java/group7/lab3/Efimov/varB5/MainFrame.java
+++ src/bsu/rfe/java/group7/lab3/Efimov/varB5/MainFrame.java
@@ -1,6 +1,7 @@
 package bsu.rfe.java.group7.lab3.Efimov.varB5;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
     private static final int WIDTH = 700;
@@ -21,7 +22,45 @@ public class MainFrame extends JFrame {
     private JTextField textFieldTo;
     private JTextField textFieldStep;
     private Box hBoxResult;
+    // Визуализатор ячеек таблицы
+    private GornerTableCellRenderer renderer = new
+            GornerTableCellRenderer();
+    // Модель данных с результатами вычислений
+    private GornerTableModel data;
+    public MainFrame(Double[] coefficients){
+        // Обязательный вызов конструктора предка
+        super("Табулирование многочлена на отрезке по схеме Горнера");
+// Запомнить во внутреннем поле переданные коэффициенты
+        this.coefficients = coefficients;
+// Установить размеры окна
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+// Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+        // Создать меню
+        JMenuBar menuBar = new JMenuBar();
+// Установить меню в качестве главного меню приложения
+        setJMenuBar(menuBar);
+// Добавить в меню пункт меню "Файл"
+        JMenu fileMenu = new JMenu("Файл");
+// Добавить его в главное меню
+        menuBar.add(fileMenu);
+// Создать пункт меню "Таблица"
+        JMenu tableMenu = new JMenu("Таблица");
+// Добавить его в главное меню
+        menuBar.add(tableMenu);
 
 
 
+
+
+
+
+
+
+
+
+
+    }
 }
