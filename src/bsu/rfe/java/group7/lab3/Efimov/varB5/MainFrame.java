ed4be033c20c0545e7f6ec21b7d28f849047f505
ValentinEfimov <valentinefimov00@gmail.com>
2020-11-08 16:02:45 +0300

    ADDED:global values

--- src/bsu/rfe/java/group7/lab3/Efimov/varB5/MainFrame.java
+++ src/bsu/rfe/java/group7/lab3/Efimov/varB5/MainFrame.java
@@ -3,5 +3,25 @@ package bsu.rfe.java.group7.lab3.Efimov.varB5;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    // Массив коэффициентов многочлена
+    private Double[] coefficients;
+    // Объект диалогового окна для выбора файлов
+// Компонент не создаѐтся изначально, т.к. может и не понадобиться
+// пользователю если тот не собирается сохранять данные в файл
+    private JFileChooser fileChooser = null;
+    // Элементы меню вынесены в поля данных класса, так как ими необходимо
+// манипулировать из разных мест
+    private JMenuItem saveToTextMenuItem;
+    private JMenuItem saveToGraphicsMenuItem;
+    private JMenuItem searchValueMenuItem;
+    // Поля ввода для считывания значений переменных
+    private JTextField textFieldFrom;
+    private JTextField textFieldTo;
+    private JTextField textFieldStep;
+    private Box hBoxResult;
+
+
 
 }
